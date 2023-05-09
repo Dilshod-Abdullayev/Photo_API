8940a593138301944a196f60028333b015b03542
Dilshod-Abdullayev <dilshodcoder6636@gmail.com>
2023-05-09 20:16:00 +0500

    pagination start

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import ThemeBtn from "./components/ThemeBtn";
 import Fetch from "./components/Fetch";
 import "antd/dist/reset.css";
 import { Theme } from "./components/Theme";
-import './App.css'
+import "./App.css";
 export default function App() {
-
   return (
     <div className="App dark">
       <ThemeBtn />
       <Fetch />
-      <Theme/>
+      <Theme />
     </div>
   );
 }
