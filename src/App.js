4c7fe936cf88233e2b83d7906e24fc2923d1e5be
Dilshod-Abdullayev <dilshodcoder6636@gmail.com>
2023-05-10 16:22:16 +0500

    dynamic route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,59 @@
-import React from "react";
-import ThemeBtn from "./components/ThemeBtn";
-import Fetch from "./components/Fetch";
-import "antd/dist/reset.css";
-import { Theme } from "./components/Theme";
-import "./App.css";
-import { useSelector } from "react-redux";
-export default function App() {
-  const themeReduc = useSelector((state) => state.darkThemeReducer.darkThemeOn);
+// import React from "react";
+// import ThemeBtn from "./components/ThemeBtn";
+// import Fetch from "./components/Fetch";
+// import "antd/dist/reset.css";
+// import "./App.css";
+// export default function App() {
+//   return (
+//     <div className="App dark">
+//       <ThemeBtn />
+//       <Fetch />
+//     </div>
+//   );
+// }
+
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  const elements = [
+    { id: 1, name: "Element 1", content: "Content 1" },
+    { id: 2, name: "Element 2", content: "Content 2" },
+    { id: 3, name: "Element 3", content: "Content 3" },
+  ];
+
   return (
-    <div className={themeReduc ? "light" : "dark  "}>
-      <ThemeBtn />
-      <Fetch />
+    <div>
+      {/* <Routes>
+        <Route path="/" element={<Home />}>
+          {elements.map((element) => (
+            <Route
+              key={element.id}
+              path={`/element/${element.id}`}
+              element={<ElementDetails element={element} />}
+            />
+          ))}
+        </Route>
+      </Routes> */}
+      {elements.map((item) => {
+        <h1 key={item.id}>{item.content}</h1>
+      })}
+      <h1>Hello</h1>
     </div>
   );
 }
+
+// function Home() {
+//   return <div>Welcome to the home page</div>;
+// }
+
+// function ElementDetails({ element }) {
+//   return (
+//     <div>
+//       <h2>{element.name}</h2>
+//       <p>{element.content}</p>
+//       <Link to="/">Back to Home</Link>
+//     </div>
+//   );
+// }
+
+export default App;
